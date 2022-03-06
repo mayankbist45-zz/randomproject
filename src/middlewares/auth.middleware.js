f94882a66505c79cc2ee7a7cbdddc844013a5108
Mayank Bist <mayankbist45@gmail.com>
2022-03-06 15:43:32 +0530

    added something

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -2,7 +2,7 @@ const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
 module.exports.auth = (req, res, next) => {
-  let { bearerHeader } = req.headers["Authorization"];
+  let bearerHeader = req.headers["Authorization"];
 
   const bearer = bearerHeader.split(" ");
   if (bearer.length !== 2 || bearer[0] != "Bearer") {
